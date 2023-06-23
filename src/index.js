054a1f9cf462d436774d50429610ff5650cac65e
Chidi Tuke <c2kproductions@gmail.com>
2023-06-23 17:32:07 -0400

    added all content to page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -7,36 +8,88 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
+  contact: {
     "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: "Copyright Great Idea! 2021",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+const navLinks = document.querySelectorAll("nav a");
+const heading = document.querySelector("h1");
+const btn = document.querySelector("button");
+const mainContentHeadings = document.querySelectorAll(".main-content h4");
+const mainContentP = document.querySelectorAll(".main-content p");
+const contactHeading = document.querySelector(".contact h4");
+const contactP = document.querySelectorAll(".contact p");
+const greatIdeaLogo = document.querySelector("header img");
+const ctaImg = document.querySelector("section #cta-img");
+const middleImg = document.querySelector("section #middle-img");
 
-console.log('project wired!')
+navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+
+greatIdeaLogo.src = "../mocks/img/logo.png";
+
+heading.textContent = "DOM IS AWESOME!";
+heading.style.fontSize = "6.2rem";
+
+btn.textContent = "Get Started";
+
+ctaImg.src = "../mocks/img/cta.png";
+middleImg.src = "../mocks/img/accent.png";
+
+mainContentHeadings[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentHeadings[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentHeadings[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentHeadings[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentHeadings[4].textContent = siteContent["main-content"]["vision-h4"];
+
+mainContentP[0].textContent = siteContent["main-content"]["features-content"];
+mainContentP[1].textContent = siteContent["main-content"]["about-content"];
+mainContentP[2].textContent = siteContent["main-content"]["services-content"];
+mainContentP[3].textContent = siteContent["main-content"]["product-content"];
+mainContentP[4].textContent = siteContent["main-content"]["vision-content"];
+
+contactHeading.textContent = siteContent["contact"]["contact-h4"];
+
+contactP[0].textContent = siteContent.contact.address;
+contactP[1].textContent = siteContent.contact.phone;
+contactP[2].textContent = siteContent["contact"]["email"];
+
+console.log(siteContent.contact.address);
+
+console.log("project wired!");
